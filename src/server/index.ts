a461229fc5b661eb7763e9c85852f244850ba539
JeDaYoshi <jedayoshi@disroot.org>
2019-10-20 01:34:04 -0400

    Add comma

--- src/server/index.ts
+++ src/server/index.ts
@@ -16,7 +16,7 @@ import websocket from './websocket'
 import passport from '../config/passport.config'
 import { verify_env } from '../utils/verifications.utils'
 
-verify_env('JWT_KEY', 'PORTALS_API_URL', 'PORTALS_API_KEY', 'APERTURE_WS_URL', 'APERTURE_WS_KEY' 'MONGO_URI', 'DISCORD_CLIENT_ID',
+verify_env('JWT_KEY', 'PORTALS_API_URL', 'PORTALS_API_KEY', 'APERTURE_WS_URL', 'APERTURE_WS_KEY', 'MONGO_URI', 'DISCORD_CLIENT_ID',
             'DISCORD_CLIENT_SECRET', 'DISCORD_CALLBACK_URL', 'DISCORD_OAUTH_ORIGINS')
 
 const app = express()
