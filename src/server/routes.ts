34a284cc6155465f38bc04a39d207cb48beddc1a
William Gibson <william@tired.io>
2020-05-30 03:37:35 +0100

    Upgrade Mesa

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -3,7 +3,7 @@ import { Application } from 'express'
 export default (app: Application) => {
 	app.use('/auth', require('../controllers/auth.controller').default)
 
-	app.use('/admin', require('../controllers/admin.controller').default)
+	// app.use('/admin', require('../controllers/admin.controller').default)
 	app.use('/internal', require('../controllers/internal.controller').default)
 
 	app.use('/user', require('../controllers/user.controller').default)
